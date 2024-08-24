2893a6c7df60ba547adf7082e1f1fcb6722fb669
wasabina67 <wasabina67@gmail.com>
2024-08-24 14:12:16 +0900

    Update 3

--- sample3.py
+++ sample3.py
@@ -1,5 +1,6 @@
-def long_function_name_with_unnecessary_length_and_complexity( arg1, arg2 , arg3 ):
-    result=arg1+arg2+arg3
-    print( result )
+def long_function_name_with_unnecessary_length_and_complexity(arg1, arg2, arg3):
+    result = arg1 + arg2 + arg3
+    print(result)
 
-long_function_name_with_unnecessary_length_and_complexity(1, 2 ,3)
+
+long_function_name_with_unnecessary_length_and_complexity(1, 2, 3)
