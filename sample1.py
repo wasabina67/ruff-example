3f015a1dc28839dd8e21eb4abf498b9231b89a7e
wasabina67 <wasabina67@gmail.com>
2024-08-24 14:11:50 +0900

    Update 1, 2

--- sample1.py
+++ sample1.py
@@ -1,9 +1,11 @@
 import os
 
+
 def list_files(directory):
     files = os.listdir(directory)
     print("Files in", directory, ":")
     for file in files:
         print(file)
+
 
 list_files("/path/to/directory")
--- sample2.py
+++ sample2.py
@@ -4,6 +4,7 @@ def greet(name):
     else:
         return "Hello, Stranger!"
 
+
 def redundant_code():
     a = 10
     if a == 10:
@@ -11,6 +12,7 @@ def redundant_code():
         print(b)
     else:
         print("This should never happen.")
+
 
 greet("Alice")
 redundant_code()
